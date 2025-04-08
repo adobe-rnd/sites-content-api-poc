fff1762c4dc45e376991d2c4da8cddfdf3da3286
Tanju Erinmez <terinmez@adobe.com>
2025-04-08 14:54:57 +0200

    added openapi generated ts files

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
-import { PagesList } from "./endpoints/pagesList";
-import { PagesFetch } from "endpoints/pagesFetch";
+import {
+	PagesList,
+	PagesFetchById,
+	PagesFetchByUrl,
+	PagesContentById,
+} from "./endpoints/pages";
 
 const app = new Hono();
-
 
 const openapi = fromHono(app, {
 	docs_url: "/",
 });
 
 openapi.get("/pages", PagesList);
-openapi.get("/pages/:pageId", PagesFetch);
-openapi.get("/pages/byUrl", PagesFetch);
-openapi.get("/pages/:pageId/content", PagesList);
+openapi.get("/pages/:pageId", PagesFetchById);
+openapi.get("/pages/byUrl", PagesFetchByUrl);
+openapi.get("/pages/:pageId/content", PagesContentById);
 
 export default app;
