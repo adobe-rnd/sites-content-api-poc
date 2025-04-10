dc4114073f58b4794c8a1aab4042888561af55a5
Tanju Erinmez <terinmez@adobe.com>
2025-04-10 11:21:30 +0200

    rework uuid

--- src/utils/aem-fetch.ts
+++ src/utils/aem-fetch.ts
@@ -8,7 +8,28 @@ export async function fetchAEMJson<T = any>(
 ): Promise<T> {
   const { host, authToken } = ctx;
   
-  const url = new URL(`/bin/querybuilder.json?type=cq:Pag&property=jcr:uuid&p.limit=1&p.hits=full&property.value=${uuid}&p.nodedepth=${depth}`, `https://${host}`);
+  const url = new URL(`/_jcr_id/${uuid}.${depth}.json`, `https://${host}`);
+  
+  const response = await fetch(url.toString(), {
+    headers: {
+      'Authorization': `Bearer ${authToken}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`AEM fetch failed: ${response.status} ${response.statusText}`);
+  }
+  return response.json();
+}
+
+export async function determineUuidByPath<T = any>(
+  ctx: AEMContext,
+  path: string
+): Promise<T> {
+  const { host, authToken } = ctx;
+  
+  const url = new URL(`${path}.1.json`, `https://${host}`);
   
   const response = await fetch(url.toString(), {
     headers: {
@@ -20,10 +41,9 @@ export async function fetchAEMJson<T = any>(
   const responseJson = await response.json();
   if (!response.ok) {
     throw new Error(`AEM fetch failed: ${response.status} ${response.statusText}`);
-  } else if (responseJson.results === 0) {
-    return null;
+  } else if (responseJson.jcr.uuid) {
+    return responseJson.jcr.uuid;
   } else {
-    return responseJson.hits[0];
+    return null;
   }
 }
-
